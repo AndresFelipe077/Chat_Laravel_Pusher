413ce97685ce5d1c2e07573ab4b4564e9376454f
AndresFelipe077 <pizoluligoa@gmail.com>
2023-09-25 15:50:09 -0500

    Fixed error in the ChatController controller because it gave an error when validating

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -46,15 +46,13 @@ class ChatController extends Controller
   {
     $data = $this->prepareStoreData($request);
 
-    if($data['user_id'] === $data['otherUserId'])
-    {
+    if ($data['userId'] === $data['otherUserId']) {
       return $this->error('You can not create a chat with your own');
     }
 
     $previousChat = $this->getPreviousChat($data['otherUserId']);
 
-    if($previousChat === null)
-    {
+    if ($previousChat === null) {
       $chat = Chat::create($data['data']);
       $chat->participants()->createMany([
         [
@@ -67,32 +65,30 @@ class ChatController extends Controller
 
       $chat->refresh()->load('lastMessage.user', 'participants.user');
       return $this->success($chat);
-
     }
 
     return $this->success($previousChat->load('lastMessage.user', 'participants.user'));
-
   }
 
   /**
    * Check if user and other user has previous chat or not
    *
-   * @param integer $otherUserId
+   * @param int $otherUserId
    * @return mixed
    */
   private function getPreviousChat(int $otherUserId): mixed
   {
+
     $userId = auth()->user()->id;
 
     return Chat::where('is_private', 1)
-      ->whereHas('participants', function ($query) use ($userId){
+      ->whereHas('participants', function ($query) use ($userId) {
         $query->where('user_id', $userId);
       })
       ->whereHas('participants', function ($query) use ($otherUserId) {
         $query->where('user_id', $otherUserId);
       })
       ->first();
-
   }
 
   /**
@@ -128,5 +124,4 @@ class ChatController extends Controller
     $chat->load('lastMessage.user', 'participants.user');
     return $this->success($chat);
   }
-
 }
